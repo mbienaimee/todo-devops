8ab3e9a1ddc3e7db44bdcd7dc29129e6f1acc2b5
BIENAIMEE Marie Reine <m.bienaimee@alustudent.com>
2025-08-01 11:44:59 +0200

    project

--- terraform/azure/variables.tf
+++ terraform/azure/variables.tf
@@ -3,24 +3,27 @@
 # Description: Defines input variables for the Terraform configuration.
 # ------------------------------------
 
-# The name of the resource group to create.
 variable "resource_group_name" {
   description = "The name of the resource group to create."
   type        = string
   default     = "todo-devops-rg"
 }
 
-# The Azure region where resources will be deployed.
 variable "location" {
   description = "The Azure region where resources will be deployed."
   type        = string
   default     = "southafricanorth"
 }
 
-# The deployment environment (e.g., staging, production).
-# This variable will be set by your GitHub Actions workflow.
 variable "environment" {
   description = "The deployment environment (e.g., staging, production)."
   type        = string
+}
+
+# Add a variable for the Azure Container Registry name.
+variable "acr_name" {
+  description = "The name of the Azure Container Registry."
+  type        = string
+  default     = "tdopsbienaimeetfstate" # This is a placeholder, you might want to use a unique name
 }
 
