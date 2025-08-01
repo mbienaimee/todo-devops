4738f412afb6b76068f3f3c39f241fe7e6546c5e
BIENAIMEE Marie Reine <m.bienaimee@alustudent.com>
2025-08-01 11:44:59 +0200

    project

--- terraform/azure/main.tf
+++ terraform/azure/main.tf
@@ -3,8 +3,6 @@
 # Description: Defines the core Azure resources for a Container App
 # ------------------------------------
 
-# This block configures the Terraform providers and the backend.
-# The 'backend' block MUST be nested inside the 'terraform' block.
 terraform {
   required_providers {
     azurerm = {
@@ -13,8 +11,6 @@ terraform {
     }
   }
 
-  # This is the backend that stores your Terraform state in a remote location.
-  # Terraform state files are critical for keeping track of your infrastructure.
   backend "azurerm" {
     resource_group_name  = "todo-devops-rg"
     storage_account_name = "tdopsbienaimeetfstate"
@@ -23,8 +19,6 @@ terraform {
   }
 }
 
-# The provider block is where we add the 'features' block to control provider behavior.
-# This is the fix for your initial error, allowing resource groups to be deleted.
 provider "azurerm" {
   features {
     resource_group {
@@ -37,22 +31,26 @@ provider "azurerm" {
 # Azure Resources
 # ------------------------------------
 
-# Define a resource group to hold all our resources
 resource "azurerm_resource_group" "rg" {
-  # The values for these variables are defined in the `variables.tf` file
-  # and will be passed in by your GitHub Actions workflow.
   name     = var.resource_group_name
   location = var.location
 }
 
-# This data block queries an existing Log Analytics Workspace by its name and resource group.
-# This assumes the workspace is already created in your subscription.
+# Add the Azure Container Registry (ACR) resource.
+# This resource is required for storing your Docker images.
+resource "azurerm_container_registry" "acr" {
+  name                = var.acr_name
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  sku                 = "Standard"
+  admin_enabled       = true # Enable admin user for easy access
+}
+
 data "azurerm_log_analytics_workspace" "la" {
   name                = "la-${var.resource_group_name}"
   resource_group_name = azurerm_resource_group.rg.name
 }
 
-# Create a Container App Environment. This is the foundation for your apps.
 resource "azurerm_container_app_environment" "app_env" {
   name                       = "cae-${var.resource_group_name}"
   location                   = azurerm_resource_group.rg.location
@@ -60,19 +58,26 @@ resource "azurerm_container_app_environment" "app_env" {
   log_analytics_workspace_id = data.azurerm_log_analytics_workspace.la.id
 }
 
-# Define the actual Container App
 resource "azurerm_container_app" "app" {
   name                         = "ca-${var.resource_group_name}"
   container_app_environment_id = azurerm_container_app_environment.app_env.id
   resource_group_name          = azurerm_resource_group.rg.name
 
-  # Enable ingress to expose the app to the internet
+  # FIX: The revision_mode is now a required argument.
+  revision_mode = "Single"
+
   ingress {
     external_enabled = true
     target_port      = 80
+
+    # FIX: A traffic_weight block is required when ingress is enabled.
+    # This sends 100% of traffic to the latest revision.
+    traffic_weight {
+      latest_revision = true
+      percentage      = 100
+    }
   }
 
-  # Define the container image and settings
   template {
     container {
       name   = "helloworld"
@@ -86,9 +91,4 @@ resource "azurerm_container_app" "app" {
 # ------------------------------------
 # Outputs
 # ------------------------------------
-
-# Output the fully qualified domain name (FQDN) of the container app
-output "container_app_fqdn" {
-  description = "The FQDN of the deployed container app."
-  value       = azurerm_container_app.app.ingress[0].fqdn
-}
+# This block is now moved to outputs.tf
