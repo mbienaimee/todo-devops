cad2dc5c8a2aa9bfe14fb7705b12bd141c3fec67
BIENAIMEE Marie Reine <m.bienaimee@alustudent.com>
2025-08-01 11:33:00 +0200

    project

--- terraform/azure/main.tf
+++ terraform/azure/main.tf
@@ -3,7 +3,8 @@
 # Description: Defines the core Azure resources for a Container App
 # ------------------------------------
 
-# Configure the Azure provider
+# This block configures the Terraform providers and the backend.
+# The 'backend' block MUST be nested inside the 'terraform' block.
 terraform {
   required_providers {
     azurerm = {
@@ -11,45 +12,25 @@ terraform {
       version = "~> 3.0"
     }
   }
-}
 
-# The provider block is where we add the features block to control provider behavior.
-provider "azurerm" {
-  features {
-    resource_group {
-      # This is the fix. It allows Terraform to destroy a resource group
-      # even if it contains resources. This is necessary for the old resource group
-      # to be cleaned up.
-      prevent_deletion_if_contains_resources = false
-    }
+  # This is the backend that stores your Terraform state in a remote location.
+  # Terraform state files are critical for keeping track of your infrastructure.
+  backend "azurerm" {
+    resource_group_name  = "todo-devops-rg"
+    storage_account_name = "tdopsbienaimeetfstate"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
   }
 }
 
-# This is the backend that stores your Terraform state in a remote location.
-# Replace the values with your own Azure storage account details.
-backend "azurerm" {
-  resource_group_name  = "todo-devops-rg"
-  storage_account_name = "tdopsbienaimeetfstate"
-  container_name       = "tfstate"
-  key                  = "terraform.tfstate"
-}
-
-# ------------------------------------
-# Input Variables
-# ------------------------------------
-
-# This is the name of your new resource group
-variable "resource_group_name" {
-  description = "The name of the resource group to create."
-  type        = string
-  default     = "todo-devops-rg"
-}
-
-# This is the Azure region where your resources will be deployed
-variable "location" {
-  description = "The Azure region where resources will be deployed."
-  type        = string
-  default     = "southafricanorth"
+# The provider block is where we add the 'features' block to control provider behavior.
+# This is the fix for your initial error, allowing resource groups to be deleted.
+provider "azurerm" {
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+  }
 }
 
 # ------------------------------------
@@ -58,13 +39,14 @@ variable "location" {
 
 # Define a resource group to hold all our resources
 resource "azurerm_resource_group" "rg" {
+  # The values for these variables are defined in the `variables.tf` file
+  # and will be passed in by your GitHub Actions workflow.
   name     = var.resource_group_name
   location = var.location
 }
 
 # This data block queries an existing Log Analytics Workspace by its name and resource group.
 # This assumes the workspace is already created in your subscription.
-# The name is constructed based on the resource group name for consistency.
 data "azurerm_log_analytics_workspace" "la" {
   name                = "la-${var.resource_group_name}"
   resource_group_name = azurerm_resource_group.rg.name
