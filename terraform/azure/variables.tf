fbe4be0fe802d7354aa0bc13ad7019625e9aa021
BIENAIMEE Marie Reine <m.bienaimee@alustudent.com>
2025-08-02 11:17:54 +0200

    fixxing codes

--- terraform/azure/variables.tf
+++ terraform/azure/variables.tf
@@ -1,13 +1,14 @@
 # terraform/azure/variables.tf
 
+# Defines the variables used in the Terraform configuration.
 variable "environment" {
-  description = "The environment name (e.g., 'production' or 'staging')"
+  description = "The environment name (e.g., 'production' or 'staging')."
   type        = string
-  default     = "production"
+  default     = "staging"
 }
 
 variable "location" {
-  description = "The Azure region for resources"
+  description = "The Azure region for resources."
   type        = string
-  default     = "southafricanorth" # Set your primary Azure region here
+  default     = "southafricanorth"
 }
