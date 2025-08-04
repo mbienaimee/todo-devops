245a7782a0a7f572785d611eb532d223c9381584
BIENAIMEE Marie Reine <m.bienaimee@alustudent.com>
2025-08-04 23:34:21 +0200

    project refining

--- terraform/azure/main.tf
+++ terraform/azure/main.tf
@@ -1,28 +1,44 @@
-# terraform/azure/main.tf
+# main.tf
 
-# Defines the core infrastructure resources using the environment variable.
+# This resource creates a resource group to hold all the other resources.
 resource "azurerm_resource_group" "rg" {
   name     = "todo-devops-${var.environment}-rg"
   location = var.location
 }
 
-resource "azurerm_service_plan" "app_plan" {
-  name                = "todo-devops-${var.environment}-app-plan"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  sku_name            = "B1"
-  os_type             = "Linux"
+# We use a data source to reference the existing Container App Environment
+# to avoid the regional quota error from the previous attempt.
+data "azurerm_container_app_environment" "existing" {
+  name                = "todo-devops-aca-env"
+  resource_group_name = "todo-devops-rg" # The existing environment is in this resource group.
 }
 
-resource "azurerm_linux_web_app" "app_service" {
-  name                = "todo-devops-app-${var.environment}"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  service_plan_id     = azurerm_service_plan.app_plan.id
+# This resource defines the front-end container app.
+resource "azurerm_container_app" "todo_frontend_app" {
+  name                         = "todo-frontend-app-${var.environment}"
+  resource_group_name          = azurerm_resource_group.rg.name
+  container_app_environment_id = data.azurerm_container_app_environment.existing.id
+  
+  # Set the revision mode to Single, as this is the simplest configuration.
+  revision_mode = "Single"
 
-  site_config {
-    application_stack {
-      node_version = "18-lts"
+  template {
+    container {
+      name  = "todo-frontend"
+      image = "mcr.microsoft.com/azuredocs/containerapps-helloworld:latest"
+      cpu   = 0.25
+      memory = "0.5Gi"
+    }
+  }
+
+  ingress {
+    external_enabled = true
+    target_port      = 80
+    
+    # This block is required and directs all traffic to the latest active revision.
+    traffic_weight {
+      percentage      = 100
+      latest_revision = true
     }
   }
 }
--- terraform/azure/providers.tf
+++ terraform/azure/providers.tf
@@ -1,28 +1,23 @@
-# This file defines the providers and their versions.
+# terraform/azure/providers.tf
+
+# Configure the Azure provider.
+# The subscription_id is now referenced from the variables.tf file.
 terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.80.0"
+      version = "~> 3.0"
     }
   }
 }
 
-# The rest of your provider configuration goes here
-# This provider block now explicitly uses a variable for the subscription ID.
+# Provider configuration for Azure.
+# The subscription ID is passed from the variable defined in variables.tf.
 provider "azurerm" {
   features {}
   subscription_id = var.azure_subscription_id
-
-  # This flag prevents Terraform from trying to register all
-  # supported resource providers. This fixes the error you
-  # were seeing with Microsoft.Media and Microsoft.TimeSeriesInsights.
+  
+  # This flag prevents Terraform from trying to automatically register all resource providers,
+  # which resolves the "Cannot register providers" error.
   skip_provider_registration = true
-}
-
-# Define the variable that will hold your Azure subscription ID.
-# This variable is referenced in the provider block above.
-variable "azure_subscription_id" {
-  type        = string
-  description = "The Azure subscription ID to deploy to."
 }
